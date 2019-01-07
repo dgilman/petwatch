ff7b7552105c136f6ec7122e3320875f73488f24
David Gilman <davidgilman1@gmail.com>
2019-01-07 09:38:18 -0600

    properly assert empty pet pages

--- petwatch.py
+++ petwatch.py
@@ -159,7 +159,7 @@ class PetFinder(object):
         pet_url = pet[0][0].attrib['href'].strip()
         if not pet_url.startswith('https://'):
             pet_url = 'https://' + pet_url
-        if len(pet[0][0]) == 0:
+        if len(pet[0][0]) < 2:
             return
         img_src = pet[0][0][1].attrib['src'].strip()
         if not img_src.startswith('https://'):
